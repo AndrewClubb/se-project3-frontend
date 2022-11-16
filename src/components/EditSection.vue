beeca82ac8a725b5d0050681cd0a2f59c9c08882
caspcole <caspiancoleman@gmail.com>
2022-11-16 00:44:51 -0600

    added questionable put functionality

--- src/components/EditSection.vue
+++ src/components/EditSection.vue
@@ -95,102 +95,148 @@
             </v-btn>
           </v-time-picker>
         </v-dialog>
-      </v-row>
+      
 
-      <v-col justify="space-around">
-        <strong>Start Date: </strong><br/>
-        
-        <v-menu
-          ref="startDateMenu"
-          v-model="startDateMenu"
-          :close-on-content-click="false"
-          :return-value.sync="newStartDate"
-          transition="scale-transition"
-          offset-y
-          min-width="auto"
-        >
-          <template v-slot:activator="{ on, attrs }">
-            <v-text-field
-              v-model="newStartDate"
-              label="Picker in menu"
-              prepend-icon="mdi-calendar"
-              readonly
-              v-bind="attrs"
-              v-on="on"
-            ></v-text-field>
-          </template>
-          <v-date-picker
+      <strong>Start Date: </strong><br/>
+      
+      <v-menu
+        ref="startDateMenu"
+        v-model="startDateMenu"
+        :close-on-content-click="false"
+        :return-value.sync="newStartDate"
+        transition="scale-transition"
+        offset-y
+        min-width="auto"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-text-field
             v-model="newStartDate"
-            no-title
-            scrollable
-          >
-            <v-spacer></v-spacer>
-            <v-btn
-              text
-              color="primary"
-              @click="startDateMenu = false"
-            >
-              Cancel
-            </v-btn>
-            <v-btn
-              text
-              color="primary"
-              @click="$refs.startDateMenu.save(newStartDate)"
-            >
-              OK
-            </v-btn>
-          </v-date-picker>
-        </v-menu>
-
-        <!-- <v-date-picker
-          v-model="sectionTime.startDate"
-        ></v-date-picker> -->
-        <strong>End Date: </strong><br/>
-        <v-menu
-          ref="endDateMenu"
-          v-model="endDateMenu"
-          :close-on-content-click="false"
-          :return-value.sync="newEndDate"
-          transition="scale-transition"
-          offset-y
-          min-width="auto"
+            label="Picker in menu"
+            prepend-icon="mdi-calendar"
+            readonly
+            v-bind="attrs"
+            v-on="on"
+          ></v-text-field>
+        </template>
+        <v-date-picker
+          v-model="newStartDate"
+          no-title
+          scrollable
         >
-          <template v-slot:activator="{ on, attrs }">
-            <v-text-field
-              v-model="newEndDate"
-              label="Picker in menu"
-              prepend-icon="mdi-calendar"
-              readonly
-              v-bind="attrs"
-              v-on="on"
-            ></v-text-field>
-          </template>
-          <v-date-picker
-            v-model="newEndDate"
-            no-title
-            scrollable
+          <v-spacer></v-spacer>
+          <v-btn
+            text
+            color="primary"
+            @click="startDateMenu = false"
           >
-            <v-spacer></v-spacer>
-            <v-btn
-              text
-              color="primary"
-              @click="endDateMenu = false"
-            >
-              Cancel
-            </v-btn>
-            <v-btn
-              text
-              color="primary"
-              @click="$refs.endDateMenu.save(newEndDate)"
-            >
-              OK
-            </v-btn>
-          </v-date-picker>
-        </v-menu>
-        <!-- <v-date-picker
-          v-model="sectionTime.endDate"
-        ></v-date-picker> -->
-      </v-col>
+            Cancel
+          </v-btn>
+          <v-btn
+            text
+            color="primary"
+            @click="$refs.startDateMenu.save(newStartDate)"
+          >
+            OK
+          </v-btn>
+        </v-date-picker>
+      </v-menu>
+
+      <!-- <v-date-picker
+        v-model="sectionTime.startDate"
+      ></v-date-picker> -->
+      <strong>End Date: </strong><br/>
+      <v-menu
+        ref="endDateMenu"
+        v-model="endDateMenu"
+        :close-on-content-click="false"
+        :return-value.sync="newEndDate"
+        transition="scale-transition"
+        offset-y
+        min-width="auto"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-text-field
+            v-model="newEndDate"
+            label="Picker in menu"
+            prepend-icon="mdi-calendar"
+            readonly
+            v-bind="attrs"
+            v-on="on"
+          ></v-text-field>
+        </template>
+        <v-date-picker
+          v-model="newEndDate"
+          no-title
+          scrollable
+        >
+          <v-spacer></v-spacer>
+          <v-btn
+            text
+            color="primary"
+            @click="endDateMenu = false"
+          >
+            Cancel
+          </v-btn>
+          <v-btn
+            text
+            color="primary"
+            @click="$refs.endDateMenu.save(newEndDate)"
+          >
+            OK
+          </v-btn>
+        </v-date-picker>
+      </v-menu>
+
+      <div class="form-group">
+        <strong>Room Number: </strong><br/>
+        <input
+          type="roomNumber"
+          class="form-control"
+          id="roomNumber"
+          v-model="room.number"
+        />
+      </div>
+
+      <v-container fluid>
+        <strong>Meeting Days for this Section: </strong>
+        <!-- <p>{{ selected }}</p> -->
+        <v-checkbox
+          v-model="selected"
+          label="Sunday"
+          value="Sunday"
+        ></v-checkbox>
+        <v-checkbox
+          v-model="selected"
+          label="Monday"
+          value="Monday"
+        ></v-checkbox>
+        <v-checkbox
+          v-model="selected"
+          label="Tuesday"
+          value="Tuesday"
+        ></v-checkbox>
+        <v-checkbox
+          v-model="selected"
+          label="Wednesday"
+          value="Wednesday"
+        ></v-checkbox>
+        <v-checkbox
+          v-model="selected"
+          label="Thursday"
+          value="Thursday"
+        ></v-checkbox>
+        <v-checkbox
+          v-model="selected"
+          label="Friday"
+          value="Friday"
+        ></v-checkbox>
+        <v-checkbox
+          v-model="selected"
+          label="Saturday"
+          value="Saturday"
+        ></v-checkbox>
+      </v-container>
+    </v-row>
       <div class="form-group">
         <button
           class="m-3 btn btn-sm btn-danger"
@@ -221,6 +267,7 @@
 import SectionDataService from "../services/SectionDataService";
 import SectionTimeDataService from "../services/SectionTimeDataService";
 import CourseDataService from "../services/CourseDataService";
+import RoomDataService from "../services/RoomDataService";
 
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
@@ -232,9 +279,13 @@ export default {
       section: null,
       sectionTime: null,
       course: null,
-      faculty: null,
+      // facultySections: [],
+      // relevantFacultySections: [],
+      // faculty: [],
+      // relevantFaculty: [],
       room: null,
-      activeWeekdays: [],
+      selected: [],
+      weekdays: [],
       newStartTime: null,
       newEndTime: null,
       newStartDate: null,
@@ -263,21 +314,34 @@ export default {
           let sectionTime = response.data;
 
               // load up which weekdays have classes for this section
-          if (sectionTime.sunday == 1)
-            this.activeWeekdays[0] = 1;
-          if (sectionTime.monday == 1)
-            this.activeWeekdays[1] = 1;
-          if (sectionTime.tuesday == 1)
-            this.activeWeekdays[2] = 1;
-          if (sectionTime.wednesday == 1)
-            this.activeWeekdays[3] = 1;
-          if (sectionTime.thursday == 1)
-            this.activeWeekdays[4] = 1;
-          if (sectionTime.friday == 1)
-            this.activeWeekdays[5] = 1;
-          if (sectionTime.saturday == 1)
-            this.activeWeekdays[6] = 1;
-
+          if (sectionTime.sunday == 1){
+            this.selected.push("Sunday");
+            this.weekdays[0] = 1;
+          }
+          if (sectionTime.monday == 1){
+            this.selected.push("Monday")
+            this.weekdays[1] = 1;
+          }
+          if (sectionTime.tuesday == 1){
+            this.selected.push("Tuesday")  
+            this.weekdays[2] = 1;
+          }
+          if (sectionTime.wednesday == 1){
+            this.selected.push("Wednesday")
+            this.weekdays[3] = 1;
+          }
+          if (sectionTime.thursday == 1){
+            this.selected.push("Thursday")
+            this.weekdays[4] = 1;
+          }
+          if (sectionTime.friday == 1){
+            this.selected.push("Friday")
+            this.weekdays[5] = 1;
+          }
+          if (sectionTime.saturday == 1){
+            this.selected.push("Saturday")
+            this.weekdays[6] = 1;
+          }
           this.newStartTime = sectionTime.startTime;
           this.newEndTime = sectionTime.endTime;
           this.newStartDate = sectionTime.startDate;
@@ -300,6 +364,52 @@ export default {
           console.log(e);
         });
     },
+    async getRoom(id) {
+      await RoomDataService.get(id)
+        .then((response) => {
+          this.room = response.data;
+          // console.log(response.data);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+    // async getFaculty() {
+    //   await FacultyDataService.get()
+    //     .then((response) => {
+    //       this.faculty = response.data;
+
+    //       for (let i = 0; i < this.faculty.length; i++){
+    //         for (let j = 0; j < this.relevantFacultySections.length; j++){
+    //           if (this.faculty[i].facultyId == this.relevantFacultySections[j]){
+    //             this.relevantFaculty.push(this.faculty[i])
+    //           }
+    //         }
+    //       }
+
+    //       // console.log(response.data);
+    //     })
+    //     .catch((e) => {
+    //       console.log(e);
+    //     });
+    // },
+    // async getFacultySections() {
+    //   await FacultySectionDataService.get()
+    //     .then((response) => {
+    //       this.facultySections = response.data;
+
+    //       for (let i = 0; i < this.facultySections.length; i++){
+    //         console.out(this.facultySections[i]);
+    //         if (this.facultySections[i].sectionId == this.sectionId){
+    //           this.relevantFacultySections.push(this.facultySections[i]);
+    //         }
+    //       }
+    //       // console.log(response.data);
+    //     })
+    //     .catch((e) => {
+    //       console.log(e);
+    //     });
+    // },
     updatePublished(status) {
       var data = {
         id: this.currentSection.id,
@@ -329,10 +439,28 @@ export default {
         });
     },
     updateSection() {
-      SectionDataService.update(this.currentSection.id, this.currentSection)
+      
+      var updatedObject = {
+        startTime: this.newStartTime,
+        endTime: this.newEndTime,
+        startDate: this.newStartDate,
+        endDate: this.newEndDate,
+        sunday: this.weekdays[0],
+        monday: this.weekdays[1],
+        tuesday: this.weekdays[2],
+        wednesday: this.weekdays[3],
+        thursday: this.weekdays[4],
+        friday: this.weekdays[5],
+        saturday: this.weekdays[6],
+        roomNumber: this.room.number
+      } 
+
+      
+      SectionDataService.update(this.sectionTime.id, updatedObject)
         .then((response) => {
           console.log(response.data);
-          this.retrieveSections();
+          // this.retrieveSections();
+
         })
         .catch((e) => {
           console.log(e);
@@ -355,7 +483,9 @@ export default {
     await this.getSectionTime(this.$route.params.id);
     await this.getSection(this.sectionTime.sectionId);
     await this.getCourse(this.currentSection.courseId);
-
+    await this.getRoom(this.sectionTime.roomId);
+    // await this.getFacultySections();
+    // await this.getFaculty();
   },
 };
 </script>
